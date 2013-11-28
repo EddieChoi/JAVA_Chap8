7cf23015e4e7c9694baa3a153e19f970ae97b99e
EddieChoi <faceoff9110@gmail.com>
2013-11-28 13:26:01 +0900

    Jung
    패키지Jung

---
+++ jung/Jung.java
@@ -0,0 +1,12 @@
+package kr.ac.kaywon.jung;
+
+import kr.ac.kaywon.cu.*;
+
+class Jung {
+	public static void main(String args[]){ //메인함수		
+		Test1 obj1 = new Test1();
+		obj1.a = 100;
+		obj1.b = 200;
+		obj1.c = 300;	
+	}	
+}
